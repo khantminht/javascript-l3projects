c51c4b9688b022cc99d63f1fd605ed31706f6547
khantminht <khantminht.bizs@gmail.com>
2022-09-21 09:00:07 +0630

    21LB

---
+++ lightboxslider/app.js
@@ -0,0 +1,40 @@
+var getimgs = document.querySelectorAll('.img');
+console.log(getimgs);
+
+var getmodal = document.querySelector('.modal');
+var getbtnclose = document.querySelector('.btn-close');
+var getviews = document.getElementsByClassName('view');
+
+var getprevbtn = document.querySelector('.prev');
+var getnextbtn = document.querySelector('.next');
+
+var getcounter = document.querySelector('.counter');
+var getcaption = document.querySelector('.caption');
+
+var getnoactives = document.getElementsByClassName('noactive');
+
+for(var i=0; i < getimgs.length; i++){
+    // console.log(getimgs[i]);
+
+    getimgs[i].addEventListener('click',showmodal);
+}
+
+function showmodal(){
+    getmodal.style.display = 'block';
+}
+
+// getbtnclose.addEventListener('click',function(){
+//     getmodal.style.display = 'none';
+// });
+
+getbtnclose.onclick = function(){
+    getmodal.style.display= 'none';
+}
+
+document.addEventListener('click',function(e){
+    console.log(e.target);
+
+    if(e.target === getmodal){
+        getmodal.style.display = 'none';
+    }
+});
